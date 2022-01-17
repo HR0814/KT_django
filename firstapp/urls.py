358c2316dfac992247c4e089e268871747bf7bb9
HR <yoohlee1@naver.com>
2022-01-17 14:33:47 +0900

    migration, curriculum, 데이터입력

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('main/', views.main),
+    path('insert/', views.insert),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-# from django.shortcuts import render
 from django.http import HttpResponse
+
 def index1(request):
     return HttpResponse('<u>Hello</u>')
 
@@ -12,3 +9,20 @@ def index2(request):
 
 def main(request):
     return HttpResponse('<u>Main</u>')
+
+
+import firstapp.models as models
+from .models import Curriculum
+
+def insert(request):
+    # 1 create()
+    Curriculum.objects.create(name='linux')
+    
+    # 2 save()
+    c = Curriculum(name='python')
+    c.save()
+
+    Curriculum(name='python').save()
+    Curriculum(name='django').save()
+
+    return HttpResponse('ok')
