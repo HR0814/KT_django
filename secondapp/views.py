3b6854b0baad81747e4920e44993a71c70791d9b
HR <yoohlee1@naver.com>
2022-01-17 16:03:21 +0900

    secondapp 모델, 데이터 입력, 조회

--- secondapp/views.py
+++ secondapp/views.py
@@ -5,3 +5,23 @@ from django.http import HttpResponse
 # Create your views here.
 def main(request):
     return HttpResponse('<u>Main</u>')
+
+
+# import secondapp.models as models
+from .models import Course
+
+def insert(request):
+    Course(name='데이터 분석',cnt=30).save()
+    Course(name='데이터 수집',cnt=20).save()
+    Course(name='웹개발',cnt=25).save()
+    Course(name='인공지능',cnt=20).save()
+
+    return HttpResponse('데이터 입력 완료')
+
+def show(request):
+    course = Course.objects.all()
+    result = ''
+    for c in course:
+        result += '%s %s<br>' % (c.name, c.cnt)
+
+    return HttpResponse(result)
