edd6b1db2bcd451bc6acdfd7f9f76b583051d4a0
HR <yoohlee1@naver.com>
2022-01-18 10:39:21 +0900

    firstapp overriding

--- secondapp/models.py
+++ secondapp/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Curriculum(models.Model):
+    name = models.CharField(max_length=255)
+
+    # 오버라이딩 overriding
+    def __str__(self) -> str:
+        return '@@@' + self.name
+
+
 class Course(models.Model):
     name = models.CharField(max_length=30)
     cnt = models.IntegerField()
