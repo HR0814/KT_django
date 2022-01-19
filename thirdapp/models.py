244a023cda1e8e5abb6ecd9ee1a5171e818b6c63
HR <yoohlee1@naver.com>
2022-01-19 14:02:10 +0900

    django shell

--- thirdapp/models.py
+++ thirdapp/models.py
@@ -25,3 +25,75 @@ class JejuOlle(models.Model):
   class Meta:
     db_table = 'jeju_olle'
     managed = False
+
+
+
+class Owner(models.Model):
+  name = models.CharField(max_length=50, null=True)
+  
+  class Meta:
+    db_table = 'owner'
+    managed = False
+
+class Animal(models.Model):
+  name = models.CharField(max_length=50, null=True)
+  age = models.IntegerField(null=True)
+  owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True)
+
+  class Meta:
+    db_table = 'animal'
+    managed = False
+
+class Playground(models.Model):
+  name = models.CharField(max_length=50, null=True)
+  address = models.CharField(max_length=50, null=True)
+  tel = models.CharField(max_length=20, null=True)
+  animals = models.ManyToManyField(Animal, null=True)
+
+  class Meta:
+    db_table = 'playground'
+    managed = False
+
+class Warranty(models.Model):
+  model_nm = models.CharField(max_length=50, null=True)
+  period = models.IntegerField(null=True)
+
+  class Meta:
+    db_table = 'warranty'
+    managed = False
+
+class Product(models.Model):
+  name = models.CharField(max_length=50, null=True)
+  price = models.IntegerField(null=True)
+  animal = models.ForeignKey(Animal, on_delete=models.SET_NULL, null=True)
+  warranty = models.OneToOneField(Warranty, on_delete=models.SET_NULL, null=True)
+
+  class Meta:
+    db_table = 'product'
+    managed = False
+
+
+
+
+class Dept(models.Model):
+  deptno = models.AutoField(primary_key=True)
+  dname = models.CharField(max_length=14, null=True)
+  loc = models.CharField(max_length=13, null=True)
+
+  class Meta:
+    db_table = 'dept'
+    managed = False
+
+class Emp(models.Model):
+  empno = models.AutoField(primary_key=True)
+  ename = models.CharField(max_length=10, null=True)
+  job = models.CharField(max_length=9, null=True)
+  mgr = models.IntegerField(null=True)
+  hiredate = models.DateTimeField(null=True)
+  sal = models.IntegerField(null=True)
+  comm = models.IntegerField(null=True)
+  dept = models.ForeignKey(Dept,on_delete=models.SET_NULL, null=True, db_column='deptno')
+
+  class Meta:
+    db_table = 'emp'
+    managed = False
